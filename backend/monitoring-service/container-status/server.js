8cf0065e4d7217f3fef8413643b8aaf1eab73456
2uangthor <minhquangkl04@gmail.com>
2024-11-24 20:53:37 +0700

    add again rateLimit into containerStatus

--- backend/monitoring-service/container-status/server.js
+++ backend/monitoring-service/container-status/server.js
@@ -1,13 +1,20 @@
 const Docker = require('dockerode');
 const docker = new Docker();
 const cors = require('cors');
-
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const app = express();
 
 const port = 4001;
 
 app.use(cors());
+
+// Giới hạn số lượng yêu cầu đồng thời cho mỗi dịch vụ (rate limiting)
+const logsRateLimiter = rateLimit({
+    windowMs: 10000, // 10 giây
+    max: 20, // Tối đa 20 yêu cầu mỗi 10 giây
+    message: 'Quá nhiều yêu cầu, hãy thử lại sau.'
+});
 
 async function checkContainerStatus(containerName) {
     try {
@@ -21,7 +28,7 @@ async function checkContainerStatus(containerName) {
     }
 }
 
-app.get('/status/containerStatus', async (req, res) => {
+app.get('/status/containerStatus', logsRateLimiter, async (req, res) => {
     const statuses = {
         goldPriceContainer: await checkContainerStatus('gold-price-container'),
         exchangeRateContainer: await checkContainerStatus('exchange-rate-container'),
